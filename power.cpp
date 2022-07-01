907f74f73934dd377ca705a5f623ae0b0d9d8192
Lakshay069 <21bcs9734@cuchd.in>
2022-07-01 23:48:27 +0530

    power

---
+++ power.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int pow(int m,int n){
+    if(n==0)
+    return 1;
+    if(n%2==0)
+   { return pow(m*m ,  n/2);}
+    else
+        return m*pow(m*m ,(n-1)/2);
+}
+int main(){
+    int m,n;
+    cin>>m>>n;
+    cout<<pow(m,n)<<endl;
+    return 0;
+}
