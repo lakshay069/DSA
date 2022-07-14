ffbb8aebe9616c3a657b64089f49f310aa3d5494
Lakshay069 <21bcs9734@cuchd.in>
2022-07-14 23:58:15 +0530

    power

--- power.cpp
+++ power.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int pow(int m,int n){
     if(n==0)
     return 1;
-    if(n%2==0)
-   { return pow(m*m ,  n/2);}
-    else
-        return m*pow(m*m ,(n-1)/2);
+    return pow(m ,n-1)*m;
 }
 int main(){
-    int m,n;
+    long int m,n;
     cin>>m>>n;
     cout<<pow(m,n)<<endl;
     return 0;
